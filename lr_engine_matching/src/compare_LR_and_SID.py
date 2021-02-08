af45dec39e71b20509b289caff14b456fa236263
Marianne Mullan <marianne.mullan@informa.com>
2021-02-08 12:26:52 +0000

    Updating stats.

--- lr_engine_matching/src/compare_LR_and_SID.py
+++ lr_engine_matching/src/compare_LR_and_SID.py
@@ -8,10 +8,6 @@ MATCH_THRESHOLD = 70.
 if __name__ == '__main__':
     lr_sid_to_compare_df = pd.read_csv(LR_CONFLICTS_TO_COMPARE)
 
-    print("If uploading the entire dataset without checking similaritites, there would be {} conflicts raised for "
-          "{} vessels."
-          .format(len(lr_sid_to_compare_df), len(lr_sid_to_compare_df.IMO.unique())))
-
     # Will either be deemed as the same or different. If the same, can ignore them - if different will raise a conflict
     ignore_df = lr_sid_to_compare_df.loc[lr_sid_to_compare_df.match_val >= MATCH_THRESHOLD][['IMO',
                                                                                              'ENGINE_DESIGNATION',
@@ -23,7 +19,9 @@ if __name__ == '__main__':
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
-    print('Number unique vessels that conflicts would be raised for before any matching checking is '
-          '{}'.format(len(lr_sid_to_compare_df.IMO.unique())))
-    print('Number unique vessels that conflicts would be raised for after matching checking at threshold {} is '
-          '{}'.format(MATCH_THRESHOLD, len(conflicts_df.IMO.unique())))
+    print("If uploading the entire dataset without checking similarities, there would be {} conflicts raised for "
+          "{} vessels."
+          .format(len(lr_sid_to_compare_df), len(lr_sid_to_compare_df.IMO.unique())))
+    print("If uploading the entire dataset after checking similarities at match threshold value of {}, "
+          "there would be {} conflicts raised for {} vessels."
+          .format(MATCH_THRESHOLD, len(conflicts_df), len(conflicts_df.IMO.unique())))
