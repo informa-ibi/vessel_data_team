4937d1ef8803196c223ea8481b947bb72af8f45e
Marianne Mullan <marianne.mullan@informa.com>
2021-02-19 16:26:26 +0000

    Getting LR script ready for mapped file.

--- lr_engine_matching/src/lr_to_apply_pt2.py
+++ lr_engine_matching/src/lr_to_apply_pt2.py
@@ -77,23 +77,29 @@ if __name__ == '__main__':
     to_map_df = manually_reviewed_df.loc[manually_reviewed_df['Match (Yes/No)'] ==
                                          'Yes'][engine_designations_to_map_columns]
     to_map_df['is_mapped'] = 'mapped'
+    to_map_df.columns = [col.replace('_lr', '') for col in to_map_df.columns]
     output_lr_apply_df = lr_to_apply_df.merge(to_map_df, how='left',
-                                              right_on=['ENGINE_DESIGNATION_lr',
-                                                        'CYLINDERS_ARRANGEMENT_lr',
-                                                        'NO_OF_CYLINDERS_lr', 'BORE_lr', 'STROKE_lr',
-                                                        'FUEL_TYPE_lr', 'DESIGNER_lr'],
                                               left_on=['ENGINE_DESIGNATION',
                                                        'CYLINDERS_ARRANGEMENT',
-                                                       'NO_OF_CYLINDERS', 'BORE', 'STROKE', 'FUEL_TYPE', 'DESIGNER'])
+                                                       'NO_OF_CYLINDERS', 'BORE', 'STROKE', 'FUEL_TYPE', 'DESIGNER'],
+                                              right_on=['ENGINE_DESIGNATION',
+                                                        'CYLINDERS_ARRANGEMENT',
+                                                        'NO_OF_CYLINDERS', 'BORE', 'STROKE',
+                                                        'FUEL_TYPE', 'DESIGNER'])
 
-    def prepare_output_df(row):
-        if row.is_mapped == 'mapped':
+    lr_columns = lr_to_apply_df.columns
+    df1_cols = ['IMO', 'NO_OF_ENGINE', 'ENGINE_TYPE', 'ENGINE_DESIGNATION_compare', 'POWER_KW',
+                'CYLINDER_ARRANGEMENT_compare', 'NUMBER_OF_CYLINDERS_compare', 'BORE_compare',
+                'STROKE_compare', 'ENGINE_BUILDER', 'Designer_compare', 'PLACE_OF_BUILD',
+                'FUEL_TYPE_compare', 'MAIN_OR_AUX']
 
-        else:
-            row['output_ENGINE_DESIGNATION'] = row['_ENGINE_DESIGNATION']
+    df1 = output_lr_apply_df.loc[output_lr_apply_df.is_mapped == 'mapped'][df1_cols]
+    df1.columns = [col.replace('_compare', '').upper() for col in df1.columns]
+    df2 = output_lr_apply_df.loc[output_lr_apply_df.is_mapped != 'mapped'][lr_columns]\
+        .rename(columns={'NO_OF_CYLINDERS': 'NUMBER_OF_CYLINDERS',
+                         'CYLINDERS_ARRANGEMENT': 'CYLINDER_ARRANGEMENT'})
 
-    test_df = output_lr_apply_df.copy()
-    test_df.loc[test_df.is_mapped=='mapped', 'output_ENGINE_DESIGNATION'] =
-
+    df_output = pd.concat([df1, df2])
     # TODO Figure out the columns to output and replace the other values that have been mapped
+    # TODO add Company IDs
     print("--- %s seconds ---" % (time.time() - start_time))
